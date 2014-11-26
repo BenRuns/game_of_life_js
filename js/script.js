bd58d8d1f5b7fed0022b25c85421a22c618af664
BenRuns <benthomaspatterson@gmail.com>
2014-11-25 19:44:20 -0800

    added

--- js/script.js
+++ js/script.js
@@ -224,5 +224,12 @@ document.getElementById("clear").onclick=clear_sim;
 
 document.getElementById("add_cells").onclick=add_points;
 
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-57134227-1', 'auto');
+  ga('send', 'pageview');
 
 
